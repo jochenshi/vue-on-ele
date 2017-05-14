fd61fe1248ded9f98587ce217566d49c1c954484
jochenshi <jochenshi@gmail.com>
2017-05-15 00:11:34 +0800

    add el-card

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,7 +2,21 @@
   <div>
     <h2>This is home page</h2>
     <div class="product-content">
-
+      <span v-for="i in 2">{{'asdasd' + i}}</span>
+      <el-row>
+        <el-col :span="8" v-for="o in 3">
+          <el-card>
+            <img src="" alt="">
+            <div>
+              <span>card{{o}}</span>
+              <div class="button-area">
+                <span class="time"></span>
+                <el-button class="button" type="text">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
