4b1b80093f56f289048242ef9eb3905e9ddacede
jochen <18362961791@163.com>
2017-06-13 18:05:50 +0800

    activity style

--- src/components/detailActivity/detailActivity.vue
+++ src/components/detailActivity/detailActivity.vue
@@ -6,30 +6,65 @@
         <span>xxx有限公司</span>
         <span>已认证</span>
       </div>
+    </div>
+    <div class="body-content add-margin">
       <dl>
         <dt>
-          <span><i class="fa fa-calendar-check-o"></i>活动日期</span>
+          <i class="fa fa-calendar-check-o"></i>
+          <span>活动日期</span>
           <span>2017.01.01-2017.01.02</span>
         </dt>
         <dt>
-          <span><i class="fa fa-clock-o"></i>集合时间</span>
+          <i class="fa fa-clock-o"></i>
+          <span>集合时间</span>
           <span>2017.01.01</span>
         </dt>
         <dt>
-          <span><i class="fa fa-map-marker"></i>集合地点</span>
+          <i class="fa fa-map-marker"></i>
+          <span>集合地点</span>
           <span>新街口德基广场一楼</span>
         </dt>
         <dt>
-        <div>活动详情</div>
-        <div>活动的详细内容以及相关的要求</div>
+          <i class="fa fa-flag"></i>
+          <span>活动详情</span>
+          <div>活动的详细内容以及相关的要求</div>
         </dt>
       </dl>
     </div>
-    <div class="contact-content">
+    <div class="contact-content add-margin">
+      <i class="fa fa-phone"></i>
       <span>联系方式</span>
     </div>
-    <div class="jobs-content">
-      <span>职位</span>
+    <div class="jobs-content add-margin">
+        <i class="fa fa-pencil-square-o"></i>
+        <span>职位1</span>
+        <ul class="job-ul">
+          <li>
+            <span>职业类型</span>
+            <span>歌手</span>
+          </li>
+          <li>
+            <span>性别</span>
+            <span>女</span>
+          </li>
+          <li>
+            <span>身高</span>
+            <span>180</span>
+          </li>
+          <li>
+            <span>人数</span>
+            <span>3</span>
+            <span class="remain-number">剩余3个名额</span>
+          </li>
+          <li>
+            <span>价格</span>
+            <span>800</span>
+          </li>
+          <li>
+            <span>职位要求</span>
+            <span>要求民族唱法</span>
+          </li>
+        </ul>
     </div>
   </div>
 </template>
