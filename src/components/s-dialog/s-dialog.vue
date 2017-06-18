59796a669eed0df5685162394caf2502e470dfd7
jochenshi <jochenshi@gmail.com>
2017-06-18 23:29:20 +0800

    make s_dialog construct

--- src/components/s-dialog/s-dialog.vue
+++ src/components/s-dialog/s-dialog.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <transition name="s_dialog_fade">
+    <div class="s_dialog_wrapper">
+      <div class="s_dialog">
+        <div class="s_dialog_header"></div>
+        <div class="s_dialog_body"></div>
+        <div class="s_dialog_footer"></div>
+      </div>
+    </div>
+  </transition>
 </template>
 <script>
   export default {
